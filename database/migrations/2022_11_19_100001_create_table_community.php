bf00d924dd84002dcf30a4fcabcb03ded4189c4b
stonarini <stonarini@cifpfbmoll.eu>
2022-11-22 17:11:27 +0100

    migration: create migrations for DB

---
+++ database/migrations/2022_11_19_100001_create_table_community.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Community;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->id();
+			$table->string('name');
+			$table->string('description');
+			$table->string('banner');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communities');
+    }
+};
---
+++ database/migrations/2022_11_19_100006_create_table_comment.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+			$table->string('content');
+			$table->string('image');
+			$table->foreignIdFor(User::class)->constrained();
+			$table->foreignIdFor(Post::class)->constrained();
+			$table->foreignIdFor(Comment::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
