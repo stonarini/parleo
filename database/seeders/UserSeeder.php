94c5a2daef587945075fed5ed5f7d08dc5043dd9
stonarini <stonarini@cifpfbmoll.eu>
2022-11-29 20:58:59 +0100

    chore(UserSeeder): add some comments

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
+		// Run after the CommunitySeeder
 		$communities = Community::all();
 
 		User::factory(20)->create()->each(function ($user) use ($communities) {
@@ -29,10 +30,15 @@ class UserSeeder extends Seeder
 					"user_id" => $user->id,
 					"community_id" => fake()->unique()->randomElement($communities)
 				])->create();
-			fake()->unique(reset: true)->randomElement();
+			// Faker's "randomElement" method must be resetted every time
+			// because for Faker the ids are just numbers
+ 	 		fake()->unique(reset: true)->randomElement();
 
+			// Get the user communities' ids
 		    $user_communities = array_map(fn ($c): int => $c["community_id"], $user->communities()->get()->toArray());
 			
+			// Get all post before creating new ones.
+			// This is useful later for the likes and comments
 			$posts = Post::all();
 			$ps = sizeof($posts);
 
@@ -42,7 +48,9 @@ class UserSeeder extends Seeder
 					"user_id" => $user->id, 
 					"community_id" => fake()->randomElement($user_communities)
 				])->create()->each(function ($p) {
+					// Get the tags of the community of the post
 					$all_tags = $p->community()->get()[0]->tags()->get();
+					// And assign them at random
 					$tags = fake()->randomElements($all_tags, rand(0, sizeof($all_tags)));
 					foreach($tags as $t) {
 						PostTag::factory(sizeof($tags))->state(fn ()=> [
@@ -52,7 +60,8 @@ class UserSeeder extends Seeder
 					}
 				});
 
-			// Generate likes for posts 
+			// Generate likes for posts (min: 0, max: 10)
+			// The posts do not include the ones of the users (see line 39)
 			Like::factory(rand(0, $ps > 0 ? $ps < 10 ? $ps : 10 : 0))
 				->state(fn () => ["user_id" => $user->id])
 				->create()->each(function ($l) use ($posts, $user) {
@@ -64,10 +73,13 @@ class UserSeeder extends Seeder
 				});
 			fake()->unique(reset: true)->randomElement();
 
+			// Same as we did for the post (line 39)
 			$comments = Comment::all();
 			$cs = sizeof($comments);
 
 			// Generate comments for posts 
+			// The max number will be one comment for each post of the site
+			// The posts do not include the ones of the users (see line 39)
 			Comment::factory(rand(0, sizeof($posts)))
 				->state(fn () => ["user_id" => $user->id])
 				->create()->each(function ($c) use ($posts, $user) {
@@ -79,7 +91,8 @@ class UserSeeder extends Seeder
 				});
 			fake()->unique(reset: true)->randomElement();
 
-			// Generate comments for comments
+			// Generate comments for comments (min: 0, max: 10)
+			// The comments do not include the ones of the users (see line 75)
 			Comment::factory(rand(0, $cs > 0 ? $cs < 10 ? $cs : 10 : 0))
 				->state(fn () => ["user_id" => $user->id])
 				->create()->each(function ($c) use ($comments, $user) {
@@ -92,6 +105,7 @@ class UserSeeder extends Seeder
 			fake()->unique(reset: true)->randomElement();
 
 			// Generate likes for comments
+			// The comments do not include the ones of the users (see line 75)
 			Like::factory(rand(0, $cs > 0 ? $cs < 10 ? $cs : 10 : 0))
 				->state(fn () => ["user_id" => $user->id])
 				->create()->each(function ($l) use ($comments, $user) {
