9bee82b02ab38d9614a31645692b280ceff45450
stonarini <stonarini@cifpfbmoll.eu>
2023-01-21 00:28:37 +0100

    add views to view post and delete

---
+++ resources/views/post/partials/delete-form.blade.php
@@ -0,0 +1,32 @@
+<section class="space-y-6">
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">Delete Post</h2>
+        <p class="mt-1 text-sm text-gray-600">Once your post is deleted, all of its data will be permanently deleted.
+            Before deleting your post, please download any data or information that you wish to retain.</p>
+    </header>
+
+    <x-danger-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">Delete
+        Post</x-danger-button>
+
+    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('post.delete', ['community' => $community->name, 'id' => $post->id]) }}"
+            class="p-6">
+            @csrf
+            @method('delete')
+
+            <h2 class="text-lg font-medium text-gray-900">Are you sure your want to delete your post?</h2>
+            <p class="mt-1 text-sm text-gray-600">Once your post is deleted, all of its data will be permanently
+                deleted.</p>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button class="ml-3">
+                    {{ __('Delete Post') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
+</section>
---
+++ resources/views/post/view.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto lg:px-8">
+                <form method="post" class="pb-1"
+                    action="{{ route('post.edit', ['community' => $community->name, 'id' => $post->id]) }}">
+                    @csrf
+                    @method('put')
+                    <x-primary-button>
+                        {{ __('Edit') }}
+                    </x-primary-button>
+                </form>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div>
+                        <p class="font-semibold text-md text-gray-600 leading-tight inline"> /r/{{ $community->name }}
+                        </p>
+                        <p class="text-sm text-gray-400 leading-tight inline"> - Posted by
+                            /u/{{ $post->user()->first()->name }} {{ $post->date }}</p>
+                        <h1 class="font-semibold text-gray-800 leading-tight mt-3" style="font-size: 1.75em">
+                            {{ $post->title }}
+                        </h1>
+                        <div>
+                            @if ($post->content)
+                                <p class="text-xl text-gray-700 leading-tight">
+                                    {{ $post->content }}
+                                </p>
+                            @else
+                                <img src="{{ $post->image }}" alt="">
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</x-app-layout>
