3daefa5c10f8e8a026a52d66271ff3ce998e2e26
stonarini <stonarini@cifpfbmoll.eu>
2023-01-21 00:25:37 +0100

    fix middleware

--- app/Http/Middleware/GetImage.php
+++ app/Http/Middleware/GetImage.php
@@ -4,22 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class GetImage 
+class GetImage
 {
     public function handle(Request $request, Closure $next)
     {
-		$url = substr($request->url(), strpos($request->url(), "/", 7));
-		if (Str::startsWith($url, ["/img/u/", "/img/r/"])) {
-			error_log(substr($url, 7));
+        $url = substr($request->url(), strpos($request->url(), "/", 7));
+        if (Str::startsWith($url, ["/img/u/", "/img/r/", "/img/p/"])) {
             $path = hash('sha256', substr($url, 7));
-			if (Storage::disk('local')->exists('public/'.$path.'.png')) {
-				return Storage::response(headers: ["Content-Type: image/png"], path:  'public/'.$path.'.png', name: substr($url, 7));
-			} 
-			return response(404);
-		}
+            if (Storage::disk('local')->exists("public/" . $path . '.png')) {
+                return Storage::response(headers: ["Content-Type: image/png"], path: 'public/' . $path . '.png', name: substr($url, 7));
+            }
+            return response(404);
+        }
         return $next($request);
     }
 }
